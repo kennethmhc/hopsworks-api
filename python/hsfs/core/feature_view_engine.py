06d607755b450846322fd01f83d108f5f17a793c
kenneth <kenneth@logicalclocks.com>
2024-07-24 15:21:23 +0200

    fix feature type

--- python/hsfs/core/feature_view_engine.py
+++ python/hsfs/core/feature_view_engine.py
@@ -993,7 +993,8 @@ class FeatureViewEngine:
             default_write_options.update(write_options)
         fg = feature_logging.get_feature_group(transformed)
         td_predictions = [feature for feature in fv.features if feature.label]
-        td_features = [feature for feature in fv.transformed_features if feature not in td_predictions]
+        td_predictions_names = set([feature.name for feature in td_predictions])
+        td_features = [feature_name for feature_name in fv.transformed_features if feature_name not in td_predictions_names]
         df = engine.get_instance().get_feature_logging_df(
             features_rows,
             fg=fg,
